d02a9f35436af3ab42a287bf78dd4182eb64beb4
Svetlana Vrublevskaya <svetulkavrub@gmail.com>
2020-10-14 00:12:40 +0300

    feat: implement human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    let num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    let tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+    if (number < 20) return num[number];
+    let digit = number % 10;
+    if (number < 100) return tens[Math.floor(number / 10) - 2] + (digit ? " " + num[digit] : "");
+    if (number < 1000) return num[Math.floor(number / 100)] + " hundred" + (number % 100 == 0 ? "" : " " + toReadable(number % 100));
 }
